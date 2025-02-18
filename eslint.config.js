a40f2dac929b7205fb666f73f4faadb937b92429
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-02-18 16:57:33 -0300

    remove unused assets and components, update index.html title

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
   {
@@ -16,7 +16,7 @@ export default [
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
