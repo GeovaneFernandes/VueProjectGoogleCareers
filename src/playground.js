0fd37c13a5df81faaaa867cd30a0eaa18c669aac
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-02-19 16:22:55 -0300

    feat: add Tailwind CSS support and playground example

---
+++ src/playground.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'banana', 'cherry']
+const vegetables = ['carrot', 'cucumber', 'potato']
+
+const fruitsAndVegetables = [...fruits, ...vegetables]
+console.log(fruitsAndVegetables) // ['apple', 'banana', 'cherry', 'carrot', 'cucumber', 'potato']
