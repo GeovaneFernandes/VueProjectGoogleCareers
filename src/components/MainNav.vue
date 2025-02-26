eba26d2192928acb185f86987a4a2c15960d69a5
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-02-26 15:49:01 -0300

    feat: add ActionButton component and integrate it into MainNav for improved user interaction

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -10,14 +10,21 @@
             </li>
           </ul>
         </nav>
+        <div class="ml-auto flex h-full items-center">
+          <ActionButton />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from './ActionButton.vue'
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+  },
   data() {
     return {
       company: 'Booble Careers',
