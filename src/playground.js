872d3e078fa59eac357ec04556974df550a1c293
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-02-26 14:33:45 -0300

    feat: add functionality to calculate and log squares of an array of numbers

--- src/playground.js
+++ src/playground.js
@@ -3,3 +3,12 @@ const vegetables = ['carrot', 'cucumber', 'potato']
 
 const fruitsAndVegetables = [...fruits, ...vegetables]
 console.log(fruitsAndVegetables) // ['apple', 'banana', 'cherry', 'carrot', 'cucumber', 'potato']
+
+//an array of squares
+//[1, 4, 9, 16, 25]
+const numbers = [1, 2, 3, 4, 5]
+const squares = numbers.map((number) => {
+  return number * number
+})
+
+console.log(squares)
