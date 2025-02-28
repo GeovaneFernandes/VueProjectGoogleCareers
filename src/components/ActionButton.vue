f9fc618147e4e8b465fd2f90bb852eecaf3e423b
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-02-28 16:16:56 -0300

    feat: refactor ActionButton component to support secondary styling and improve button structure

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,8 +1,5 @@
 <template>
-  <button
-    class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue"
-    @:click="handleClick()"
-  >
+  <button :class="{ secondary: true }" @click="handleClick()">
     {{ text }}
   </button>
 </template>
@@ -18,3 +15,32 @@ export default {
   props: ['text'],
 }
 </script>
+
+<style scoped>
+button {
+  border-radius: 0.375rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  font-weight: 500;
+}
+
+.primary {
+  border: 0;
+  background-color: var(--color-brand-blue-1);
+  color: white;
+  transition: box-shadow 0.2s;
+}
+
+.primary:hover {
+  box-shadow: var(--shadow-blue);
+}
+
+.secondary {
+  border: 0;
+  background-color: var(--color-brand-green-1);
+  color: white;
+  transition: box-shadow 0.2s;
+}
+</style>
